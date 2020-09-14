f829ee0d9fe898ae43a1973c28365c2673ca3301
Machalka <al1999dk@gmail.com>
2020-09-14 22:51:35 +0300

    Updates

--- precache-manifest.3f847dc009ef0684ba74d511713b7724.js
+++ precache-manifest.3f847dc009ef0684ba74d511713b7724.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a01c8920442b7b8e6f42c8f1152669ae",
+    "revision": "624aa650a5183fa8f5727e77d53df4d5",
     "url": "/VkImageEx/index.html"
   },
   {
-    "revision": "5665bc936c3f57ce284a",
+    "revision": "2d1675f96323b60c2dd6",
     "url": "/VkImageEx/static/css/2.09e62520.chunk.css"
   },
   {
-    "revision": "5665bc936c3f57ce284a",
-    "url": "/VkImageEx/static/js/2.f68839d3.chunk.js"
+    "revision": "2d1675f96323b60c2dd6",
+    "url": "/VkImageEx/static/js/2.c972e733.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/VkImageEx/static/js/2.f68839d3.chunk.js.LICENSE.txt"
+    "url": "/VkImageEx/static/js/2.c972e733.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59a1fbfb473992f2142a",
-    "url": "/VkImageEx/static/js/main.64f4ae5d.chunk.js"
+    "revision": "187855e2003149e4fc6d",
+    "url": "/VkImageEx/static/js/main.407417f6.chunk.js"
   },
   {
     "revision": "aa292717901600fcd944",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VkImageEx/precache-manifest.40d3b33e2478812832baa0cb3d0c5fb0.js"
+  "/VkImageEx/precache-manifest.3f847dc009ef0684ba74d511713b7724.js"
 );
 
 self.addEventListener('message', (event) => {
